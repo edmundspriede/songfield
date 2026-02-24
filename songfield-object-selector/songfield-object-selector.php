8c81eee5ae7d26374e47ccea122e395ddc948901
Edmunds Priede <edmunds@kls.lv>
2026-02-24 09:50:38 +0200

    Refactor object selection logic and update context
    changed to admin-ajax

--- songfield-object-selector/songfield-object-selector.php
+++ songfield-object-selector/songfield-object-selector.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * Plugin Name: Songfield Object Selector
@@ -42,47 +41,48 @@ class Songfield_Object_Selector {
         
         // Get the JWT cookie value
         $jwt_token = isset($_COOKIE['my_jwt']) ? $_COOKIE['my_jwt'] : '';
-        
         $user_id = get_current_user_id();
-        $current_object = array('id' => 333  , 'title' => 'Bērnudārzs X' );
-        
-        $children = array(  array('id' => 555  , 'title' => 'AAAA' ), array('id' => 666  , 'title' => 'BBBB'));
+        $current_object_id = get_user_meta( $user_id, 'current_object', true );
+        $current_object_title = get_the_title( $current_object_id );
+
+        $children = $this->get_related_objects($user_id);
         
         // Initial context state
         $context = [
-            'currentObjectId' => $current_object['id'],
-            'currentObjectTitle' => $current_object['title'],
+            'currentObjectId' => $current_object_id,
+            'currentObjectTitle' => $current_object_title,
             'availableObjects' => $children,
-            'isLoading' => false,
-            'isListVisible' => false,
+            'isUpdating' => false,
+            'isListHidden' => true,
             'error' => '',
             'jwtToken' => $jwt_token,
-            'apiBaseUrl' => rest_url('songfield/v1')
+            'apiBaseUrl' => admin_url('admin-ajax.php').'?action=my_objects_action'
         ];
+        
+       wp_interactivity_state( 'songfieldObjectSelector', $context );
         
         ob_start();
         ?>
         <div 
-            data-wp-interactive="songfield-object-selector"
+            data-wp-interactive="songfieldObjectSelector"
             data-wp-context='<?php echo wp_json_encode($context); ?>'
             data-wp-init="callbacks.init"
             class="songfield-object-selector"
         >
-            
-            <div class="songfield-current-container" data-wp-text="context.currentObjectTitle">
-                 
-            </div>
-            <a class="songfield-change-button" data-wp-on--click="actions.showList">
-		<span class="button-content-wrapper">
-		<span class="button-icon">
-                    <svg aria-hidden="true" class="e-font-icon-svg e-fas-exchange-alt" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M0 168v-16c0-13.255 10.745-24 24-24h360V80c0-21.367 25.899-32.042 40.971-16.971l80 80c9.372 9.373 9.372 24.569 0 33.941l-80 80C409.956 271.982 384 261.456 384 240v-48H24c-13.255 0-24-10.745-24-24zm488 152H128v-48c0-21.314-25.862-32.08-40.971-16.971l-80 80c-9.372 9.373-9.372 24.569 0 33.941l80 80C102.057 463.997 128 453.437 128 432v-48h360c13.255 0 24-10.745 24-24v-16c0-13.255-10.745-24-24-24z"></path></svg>			</span>
-		   
-		</span>
-	    </a>
-            <div class="songfield-selector-container">
-                <div data-wp-bind--hidden="!state.isListVisible">
-                    <div>
-
+         
+            <div class="songfield-current-wrapper">
+                <div class="songfield-current-container is-loading" data-wp-text="context.currentObjectTitle" data-wp-class--is-loading="context.isLoading"></div>
+                
+                <div class="songfield-change-button" data-wp-class--spin="context.isUpdating">
+                    <a  data-wp-on--click="actions.showList" class="button-spin" data-wp-class--spinning="context.isUpdating"> 
+                      
+                    </a>    
+                </div>
+            </div>    
+               
+           
+            <div class="songfield-selector-container hidden" data-wp-bind--hidden="context.isListHidden" data-wp-class--hidden="context.isListHidden">
+              
                         <ul 
                             id="object-selector"
                             class="songfield-selector-dropdown"
@@ -94,26 +94,41 @@ class Songfield_Object_Selector {
                             </template>
                         </ul>
       
-                      
-                    </div>
-                    
-                    <div 
-                        class="songfield-selector-error"
-                        data-wp-show="state.error"
-                        data-wp-text="state.error"
-                    ></div>
-                </div>
-                
-                <div 
-                    class="songfield-selector-loading"
-                    data-wp-bind--hidden="!state.isLoading"
-                >
-                    Loading objects...
-                </div>
             </div>
-        </div>
+        </div>          
+                
+        
         <?php
-        return ob_get_clean();
+        //$html = ob_get_clean();
+        //$processed_html = wp_interactivity_process_directives( $html );
+        return  ob_get_clean();
+    }
+    
+    private function get_related_objects( $user_id)  {
+        
+        if ( function_exists( 'jet_engine' ) ) {
+
+           $children = array();
+           $relation = jet_engine()->relations->get_active_relations( 7 );
+
+           if ( $relation ) {
+
+              // Get children where parent = current user
+              $related_items = $relation->get_children( $user_id );
+
+            if ( ! empty( $related_items ) ) {
+                foreach ( $related_items as $child ) {
+                    $children[] = [
+                        'id'    => $child['child_object_id'],
+                        'title' => get_the_title( $child['child_object_id'] ),
+                    ];
+                }
+            }
+        }
+        
+        return $children;
+       } else  return false;
+    
     }
 }
 
